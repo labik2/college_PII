fd9eac2ddb89c40518470c9770781e0428b84b4f
labik2 <olabinskyi2021@itcollege.lviv.ua>
2025-04-27 17:04:55 +0300

    Update main.js

--- lab11/main.js
+++ lab11/main.js
@@ -10,51 +10,43 @@ console.log(c);
 
 // Завдання 2
 function testScope() {
-    if (true) {
-        var x = 5;
-        let y = 10;
-        const z = 15;
-    }
-    console.log(x); 
-    console.log(y);
-    console.log(z); 
-}
-testScope();
+      if (true) {
+          var x = 5;
+          let y = 10;
+          const z = 15;
+          console.log(y);
+          console.log(z);
+      }
+      console.log(x);
+  }
+  testScope();
 
 // Завдання 3
-console.log(5 + "5"); 
+console.log(5 + "5");
 console.log("5" - 2);
-console.log(true + false); 
-console.log(null + 1); 
-console.log(undefined + 1); 
-console.log(0 == false); 
-console.log(0 === false); 
+console.log(true + false);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(0 == false);
+console.log(0 === false);
 
 // Завдання 4
-const person = {
-    name: "John",
-    age: 30
-};
-
+const person = { name: "John", age: 30 };
 
 person.age = 31;
 person.city = "New York";
 console.log(person);
 
-
-
-
 Object.freeze(person);
 
-
-person.age = 40; 
+person.age = 40;
 console.log(person);
 
 // Завдання 5
 function checkType(value) {
-    return typeof value;
-}
-
-console.log(checkType(10));     
-console.log(checkType("Hello"));
-console.log(checkType(null));    
+      return typeof value;
+  }
+  
+  console.log(checkType(10));
+  console.log(checkType("Hello"));
+  console.log(checkType(null));
