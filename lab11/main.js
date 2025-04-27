3e34c49774ea5d93f708c04cc23b8fa0fa393d81
labik2 <olabinskyi2021@itcollege.lviv.ua>
2025-04-27 16:52:43 +0300

    Update main.js

--- lab11/main.js
+++ lab11/main.js
@@ -1,12 +1,12 @@
 // Завдання 1
-console.log(a); 
 var a = 10;
-
-console.log(b); 
 let b = 20;
-
-console.log(c); 
 const c = 30;
+
+
+console.log(a); 
+console.log(b); 
+console.log(c); 
 
 // Завдання 2
 function testScope() {
