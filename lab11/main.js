2d5d084226540f27ba838378b345d0a73585f1bc
labik2 <olabinskyi2021@itcollege.lviv.ua>
2025-04-27 16:37:37 +0300

    Add files via upload

---
+++ lab11/main.js
@@ -0,0 +1,60 @@
+// Завдання 1
+console.log(a); 
+var a = 10;
+
+console.log(b); 
+let b = 20;
+
+console.log(c); 
+const c = 30;
+
+// Завдання 2
+function testScope() {
+    if (true) {
+        var x = 5;
+        let y = 10;
+        const z = 15;
+    }
+    console.log(x); 
+    console.log(y);
+    console.log(z); 
+}
+testScope();
+
+// Завдання 3
+console.log(5 + "5"); 
+console.log("5" - 2);
+console.log(true + false); 
+console.log(null + 1); 
+console.log(undefined + 1); 
+console.log(0 == false); 
+console.log(0 === false); 
+
+// Завдання 4
+const person = {
+    name: "John",
+    age: 30
+};
+
+
+person.age = 31;
+person.city = "New York";
+console.log(person);
+
+
+
+
+Object.freeze(person);
+
+
+person.age = 40; 
+console.log(person);
+
+// Завдання 5
+function checkType(value) {
+    return typeof value;
+}
+
+console.log(checkType(10));     
+console.log(checkType("Hello"));
+console.log(checkType(null));    
