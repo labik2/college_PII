dff9cf4c3b576ae63c1b4ba033ac40544734afc9
unknown <olabinskyi2021@itcollege.lviv.ua>
2025-05-10 15:33:37 +0300

    pw6

---
+++ pw6/city-times-app/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+  },
+};
